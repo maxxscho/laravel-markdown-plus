777698b362575957f777dcb1e76e522c8e1f4500
Markus Schober <maxxscho@inode.at>
2014-07-22 21:24:47 +0200

    Remove unused method in DocumentTest
    Remove method 'createApplication' in DocumentTest.php and add method 'tearDown'

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -14,20 +14,9 @@ class DocumentTest extends PHPUnit_Framework_TestCase
 
 
 
-    /**
-     * Creates the application.
-     *
-     * Needs to be implemented by subclasses.
-     *
-     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
-     */
-    public function createApplication()
+    public function tearDown()
     {
-        $unitTesting = true;
-
-        $testEnvironment = 'testing';
-
-        return require __DIR__ . '/../../../../bootstrap/start.php';
+        M::close();
     }
 
 
